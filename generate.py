fdbf7cd54080b9431dab88bcff536a5f49134bca
salwil <salome.wildermuth@gmail.com>
2019-05-19 16:12:00 +0200

    comments added

--- generate.py
+++ generate.py
@@ -24,7 +24,16 @@ import random
 """
 
 class NGramModel:
+
     def __init__(self, path_to_document: str, nr_of_lines: int, n_gram_size: int, inf_loop=False):
+        """
+        Class NGramModel reads a file and generates on base of the probability of n-gram sequences new songtext lines.
+        :param path_to_document:    Path to input file, str
+        :param nr_of_lines:         number of songtext lines to be generated, int
+        :param n_gram_size:         size of n_grams on base of which we calculate the probabilites for follower tokens,
+                                    int
+        :param inf_loop:            True, if we don't want to stop generating songtext lines anymore; else False, bool
+        """
         self.input = self.extract_input_file(path_to_document, n_gram_size)
         self._ngram_freqs = self.get_ngram_freqs(self.input, n_gram_size)
         if inf_loop:
@@ -35,6 +44,14 @@ class NGramModel:
                 print(' '.join(self.generate_sentence(self._ngram_freqs, n_gram_size)))
 
     def extract_input_file(self, path_to_document: str, n_gram_size: int):
+        """
+        Extracting inputfile and return all tokens in a list. Each BOS we mark with as many <s> as n_gram_size,
+        transmitted by the user. This is necessary for the generation of ngrams. Each EOS we mark with </s>.
+        :param path_to_document:    Path to input file, str
+        :param n_gram_size:         size of n_grams on base of which we calculate the probabilites for follower tokens,
+                                    int
+        :return:                    Tokenlist, lst
+        """
         token_lst = ['<s>' for i in range(0,n_gram_size-1)]
         with open(path_to_document, 'r') as f:
             for line in f:
@@ -48,7 +65,11 @@ class NGramModel:
     @staticmethod
     def get_ngram_freqs(tokens: List[str], n_gram_size: int):
         """
-        Returns the ngrams the inputlines contain, alongside their frequency, as a dictionary.
+        :param tokens:              All tokens from the inputfile in a list, lst
+        :param n_gram_size:         size of n_grams on base of which we calculate the probabilites for follower tokens,
+                                    int
+        :return:                    Returns the ngrams the inputlines contain, alongside their frequency, as a two-
+                                    dimensional dictionary, Dict[tuple, Dict[str, float]]
         """
         n_gram_lst = list(ngrams(tokens, n_gram_size))
         freq = defaultdict(lambda: defaultdict(lambda: 0.0))
@@ -60,6 +81,14 @@ class NGramModel:
         return freq
 
     def generate_sentence(self, freq: Dict[tuple, Dict[str, float]], n_gram_size: int):
+        """
+
+        :param freq:                The ngrams the inputlines contain, alongside their frequency, as a two-dimensional
+                                    dictionary, Dict[tuple, Dict[str, float]]
+        :param n_gram_size:         size of n_grams on base of which we calculate the probabilites for follower tokens,
+                                    int
+        :return:                    A generated songtext line, lst
+        """
         sum_of_fitness = 0
         hist_lst = ['<s>' for i in range(0,n_gram_size-1)]
         hist_tup = tuple(hist_lst)
